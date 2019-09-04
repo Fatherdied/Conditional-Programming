229033fa178e4cea1abebf5b5f0b70e2fb673a1f
Fatherdied <54420528+Fatherdied@users.noreply.github.com>
2019-09-04 15:13:21 -0700

    Add files via upload

---
+++ Polynomial.java
@@ -0,0 +1,19 @@
+/**
+ * Skeleton program for Week 4 Lab on Operators and Conditional Branching.
+ *
+ * @author Roy Chancellor
+ * @version 2/6/2019
+ */
+public class Polynomial
+{
+    public static void main( String[] args ) {
+        //variable declarations
+        double a = 1;
+        double p;
+        //Polynomial computation
+        p = 2*a*a -7*a + 3;
+        
+        //Output the result
+        System.out.println("p("+ a +") = " + p);
+    }
+}
